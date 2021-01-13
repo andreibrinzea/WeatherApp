c52a864b5899024c3e0e158f5ed9bd0928358f2f
Andrei Brinzea <andrei.brinzea@mta.ro>
2021-01-13 22:28:42 +0200

    Adaugare clasa ModelSuiteClasses
    A fost adaugata clasa de testare ModelSuiteClasses, clasa
    care combina clasele de testare:CityTest, CountryTest si
    CountryNamesTest intr-o suita de teste.
    
    Signed-off-by: Andrei Brinzea <andrei.brinzea@mta.ro>

---
+++ src/test/java/ro/mta/se/lab/model/ModelSuiteClasses.java
@@ -0,0 +1,13 @@
+package ro.mta.se.lab.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CityTest.class,
+        CountryTest.class,
+        CountryNamesTest.class})
+
+public class ModelSuiteClasses {
+}
