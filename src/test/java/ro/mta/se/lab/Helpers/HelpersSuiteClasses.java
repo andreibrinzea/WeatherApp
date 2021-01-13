2625f0885889401e30e4259679faa6ec8c1c23e2
Andrei Brinzea <andrei.brinzea@mta.ro>
2021-01-13 22:39:10 +0200

    Adaugare clasa HelpersSuiteClasses
    A fost adaugata clasa de testare HelpersSuiteClasses,
    clasa care combina clasele de testare: WeatherClientTest
    si ParserTest intr-o suita de teste.
    
    Signed-off-by: Andrei Brinzea <andrei.brinzea@mta.ro>

---
+++ src/test/java/ro/mta/se/lab/Helpers/HelpersSuiteClasses.java
@@ -0,0 +1,12 @@
+package ro.mta.se.lab.Helpers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ParserTest.class,
+        WeatherClientTest.class})
+
+public class HelpersSuiteClasses {
+}
